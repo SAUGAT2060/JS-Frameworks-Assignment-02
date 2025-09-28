cccf24b00e091d9608171a92414be8746f764576
Saugat Rana <ranasaugat9@gmail.com>
2025-09-28 10:42:11 -0400

    Finishing Touch

--- script1.js
+++ script1.js
@@ -1,9 +1,15 @@
-
+// We will be using EJS ( new module for this assignment )
+//i.e Import instead of require 
 import * as imports from './script2.js'
-imports.Intro("Georgian@Illac", "Rana")
-console.log(imports.toFahrenheit(2.6))
-console.log(typeof imports.toFahrenheit(2.6))
-console.log(imports.square(5));
+
+//Utilizing the imports/functions we imported form the script2.js file
+imports.Intro("Georgian@Illac", "Rana")//First function
+
+console.log(imports.toFahrenheit(2.6))//Second function
+console.log(typeof imports.toFahrenheit(2.6))// Double checking the type just to make sure
+
+console.log(imports.square(5));//Third function
 console.log(imports.square(2.5));
-console.log(imports.maxOfTwo(10, 7));
+
+console.log(imports.maxOfTwo(10, 7));//Fourth function
 console.log(imports.maxOfTwo(3, 9));  
--- script2.js
+++ script2.js
@@ -1,15 +1,16 @@
+//First Function
 export function Intro(School,Name){
   console.log("Welcome to " + School + " Mr " + Name);
 }
-
+//Second Function
 export function toFahrenheit(celsius) {
   return (celsius * 9/5) + 32;
 }
-
+//Third Function
 export function square(n) {
   return n * n;
 }
-
+//Fourth Function
 export function maxOfTwo(a, b) {
   if(a>b)
   {
